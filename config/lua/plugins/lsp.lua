0d2c08d61c694cd80d82c424305c5180f4ec1b90
Bill Xing <bhx@google.com>
2025-02-19 03:00:47 +0000

    added trouble.nvim

--- config/lua/plugins/lsp.lua
+++ config/lua/plugins/lsp.lua
@@ -19,11 +19,8 @@ return {
             if client.workspace_folders then
               local path = client.workspace_folders[1].name
               if
-                  path ~= vim.fn.stdpath("config")
-                  and (
-                    vim.loop.fs_stat(path .. "/.luarc.json")
-                    or vim.loop.fs_stat(path .. "/.luarc.jsonc")
-                  )
+                path ~= vim.fn.stdpath("config")
+                and (vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc"))
               then
                 return
               end
@@ -95,7 +92,7 @@ return {
       -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
       -- See the full "keymap" documentation for information on defining your own keymap.
       keymap = {
-        preset = "enter"
+        preset = "enter",
       },
 
       appearance = {
@@ -119,10 +116,10 @@ return {
         },
         menu = {
           draw = {
-            treesitter = { 'lsp' },
-            columns = { { 'kind_icon' }, { 'label', 'label_description', gap = 1 }, { 'source_name' } },
-          }
-        }
+            treesitter = { "lsp" },
+            columns = { { "kind_icon" }, { "label", "label_description", gap = 1 }, { "source_name" } },
+          },
+        },
       },
 
       signature = {
@@ -137,6 +134,43 @@ return {
     },
     build = "cargo build --release",
     opts_extend = { "sources.default" },
+  },
+  {
+    "folke/trouble.nvim",
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
   },
   {
     "stevearc/conform.nvim",
