4fb8103b91104a72734a6e86db72f09ede21836e
Bill Xing <bhx@google.com>
2025-02-19 00:17:28 +0000

    optimize lsp options.

--- config/lua/config/options.lua
+++ config/lua/config/options.lua
@@ -1,5 +1,5 @@
 vim.opt.autoindent = true
-vim.expandtab = true
+vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.mouse = "a"
--- config/lua/plugins/lsp.lua
+++ config/lua/plugins/lsp.lua
@@ -1,161 +1,184 @@
 return {
-	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			{
-				"folke/neoconf.nvim",
-				cmd = "Neoconf",
-				opts = {},
-			},
-			{ "saghen/blink.cmp" },
-		},
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      {
+        "folke/neoconf.nvim",
+        cmd = "Neoconf",
+        opts = {},
+      },
+      { "saghen/blink.cmp" },
+    },
 
-		-- example using `opts` for defining servers
-		opts = {
-			servers = {
-				lua_ls = {
-					on_init = function(client)
-						if client.workspace_folders then
-							local path = client.workspace_folders[1].name
-							if
-									path ~= vim.fn.stdpath("config")
-									and (
-										vim.loop.fs_stat(path .. "/.luarc.json")
-										or vim.loop.fs_stat(path .. "/.luarc.jsonc")
-									)
-							then
-								return
-							end
-						end
+    -- example using `opts` for defining servers
+    opts = {
+      servers = {
+        lua_ls = {
+          on_init = function(client)
+            if client.workspace_folders then
+              local path = client.workspace_folders[1].name
+              if
+                  path ~= vim.fn.stdpath("config")
+                  and (
+                    vim.loop.fs_stat(path .. "/.luarc.json")
+                    or vim.loop.fs_stat(path .. "/.luarc.jsonc")
+                  )
+              then
+                return
+              end
+            end
 
-						client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-							runtime = {
-								-- Tell the language server which version of Lua you're using
-								-- (most likely LuaJIT in the case of Neovim)
-								version = "LuaJIT",
-							},
-							-- Make the server aware of Neovim runtime files
-							workspace = {
-								checkThirdParty = false,
-								library = {
-									vim.env.VIMRUNTIME,
-									-- Depending on the usage, you might want to add additional paths here.
-									-- "${3rd}/luv/library"
-									-- "${3rd}/busted/library",
-								},
-								-- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
-								-- library = vim.api.nvim_get_runtime_file("", true)
-							},
-						})
-					end,
-					settings = {
-						Lua = {},
-					},
-				},
-			},
-		},
-		config = function(_, opts)
-			local lspconfig = require("lspconfig")
-			local lsp_configs = require("lspconfig.configs")
+            client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+              runtime = {
+                -- Tell the language server which version of Lua you're using
+                -- (most likely LuaJIT in the case of Neovim)
+                version = "LuaJIT",
+              },
+              -- Make the server aware of Neovim runtime files
+              workspace = {
+                checkThirdParty = false,
+                library = {
+                  vim.env.VIMRUNTIME,
+                  -- Depending on the usage, you might want to add additional paths here.
+                  -- "${3rd}/luv/library"
+                  -- "${3rd}/busted/library",
+                },
+                -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+                -- library = vim.api.nvim_get_runtime_file("", true)
+              },
+            })
+          end,
+          settings = {
+            Lua = {},
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      local lspconfig = require("lspconfig")
+      local lsp_configs = require("lspconfig.configs")
 
-			for server, config in pairs(neoroedeer.extra_options.extra_lsps) do
-				lsp_configs[server] = config
-			end
+      for server, config in pairs(neoroedeer.extra_options.extra_lsps) do
+        lsp_configs[server] = config
+      end
 
-			local config_by_table = function(t)
-				for server, config in pairs(t) do
-					-- passing config.capabilities to blink.cmp merges with the capabilities in your
-					-- `opts[server].capabilities, if you've defined it
-					config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
-					lspconfig[server].setup(config)
-				end
-			end
-			config_by_table(opts.servers)
-			config_by_table(neoroedeer.extra_options.extra_lsps_opts)
-		end,
-	},
-	{
-		"saghen/blink.cmp",
-		-- optional: provides snippets for the snippet source
-		dependencies = "rafamadriz/friendly-snippets",
+      local config_by_table = function(t)
+        for server, config in pairs(t) do
+          -- passing config.capabilities to blink.cmp merges with the capabilities in your
+          -- `opts[server].capabilities, if you've defined it
+          config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+          lspconfig[server].setup(config)
+        end
+      end
+      config_by_table(opts.servers)
+      config_by_table(neoroedeer.extra_options.extra_lsps_opts)
+    end,
+  },
+  {
+    "saghen/blink.cmp",
+    -- optional: provides snippets for the snippet source
+    dependencies = "rafamadriz/friendly-snippets",
 
-		-- use a release tag to download pre-built binaries
-		version = "*",
-		-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-		-- build = 'cargo build --release',
-		-- If you use nix, you can build from source using latest nightly rust with:
-		-- build = 'nix run .#build-plugin',
+    -- use a release tag to download pre-built binaries
+    version = "*",
+    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+    -- build = 'cargo build --release',
+    -- If you use nix, you can build from source using latest nightly rust with:
+    -- build = 'nix run .#build-plugin',
 
-		---@module 'blink.cmp'
-		---@type blink.cmp.Config
-		opts = {
-			-- 'default' for mappings similar to built-in completion
-			-- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-			-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-			-- See the full "keymap" documentation for information on defining your own keymap.
-			keymap = { preset = "default" },
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      -- 'default' for mappings similar to built-in completion
+      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+      -- See the full "keymap" documentation for information on defining your own keymap.
+      keymap = {
+        preset = "enter"
+      },
 
-			appearance = {
-				-- Sets the fallback highlight groups to nvim-cmp's highlight groups
-				-- Useful for when your theme doesn't support blink.cmp
-				-- Will be removed in a future release
-				use_nvim_cmp_as_default = true,
-				-- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-				-- Adjusts spacing to ensure icons are aligned
-				nerd_font_variant = "mono",
-			},
+      appearance = {
+        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+        -- Useful for when your theme doesn't support blink.cmp
+        -- Will be removed in a future release
+        -- use_nvim_cmp_as_default = true,
+        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = "mono",
+      },
 
-			-- Default list of enabled providers defined so that you can extend it
-			-- elsewhere in your config, without redefining it, due to `opts_extend`
-			sources = {
-				default = { "lsp", "path", "snippets", "buffer" },
-			},
-		},
-		build = "cargo build --release",
-		opts_extend = { "sources.default" },
-	},
-	{
-		"stevearc/conform.nvim",
-		event = { "BufWritePre" },
-		cmd = { "ConformInfo" },
-		keys = {
-			{
-				-- Customize or remove this keymap to your liking
-				"<leader>f",
-				function()
-					require("conform").format({ async = true })
-				end,
-				mode = "",
-				desc = "Format buffer",
-			},
-		},
-		-- This will provide type hinting with LuaLS
-		---@module "conform"
-		---@type conform.setupOpts
-		opts = {
-			-- Define your formatters
-			formatters_by_ft = {
-				lua = { "stylua" },
-				-- python = { "isort", "black" },
-				-- javascript = { "prettierd", "prettier", stop_after_first = true },
-			},
-			-- Set default options
-			default_format_opts = {
-				lsp_format = "fallback",
-			},
-			-- Set up format-on-save
-			format_on_save = { timeout_ms = 500 },
-			-- Customize formatters
-			formatters = {
-				shfmt = {
-					prepend_args = { "-i", "2" },
-				},
-			},
-		},
-		init = function()
-			-- If you want the formatexpr, here is the place to set it
-			vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-		end,
-	},
+      completion = {
+        documentation = {
+          auto_show = true,
+        },
+        list = {
+          selection = {
+            preselect = false,
+          },
+        },
+        menu = {
+          draw = {
+            treesitter = { 'lsp' },
+            columns = { { 'kind_icon' }, { 'label', 'label_description', gap = 1 }, { 'source_name' } },
+          }
+        }
+      },
+
+      signature = {
+        enabled = true,
+      },
+
+      -- Default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, due to `opts_extend`
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
+    },
+    build = "cargo build --release",
+    opts_extend = { "sources.default" },
+  },
+  {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+      {
+        -- Customize or remove this keymap to your liking
+        "<leader>f",
+        function()
+          require("conform").format({ async = true })
+        end,
+        mode = "",
+        desc = "Format buffer",
+      },
+    },
+    -- This will provide type hinting with LuaLS
+    ---@module "conform"
+    ---@type conform.setupOpts
+    opts = {
+      -- Define your formatters
+      formatters_by_ft = {
+        lua = { "stylua" },
+        -- python = { "isort", "black" },
+        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      },
+      -- Set default options
+      default_format_opts = {
+        lsp_format = "fallback",
+      },
+      -- Set up format-on-save
+      format_on_save = { timeout_ms = 500 },
+      -- Customize formatters
+      formatters = {
+        shfmt = {
+          prepend_args = { "-i", "2" },
+        },
+      },
+    },
+    init = function()
+      -- If you want the formatexpr, here is the place to set it
+      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
+  },
 }
